88240aad26563921fcdfe23cd58719faf1e01e63
Sosin Dima <sosindima5@gmail.com>
2024-02-22 20:40:20 +0700

    fix swiper, fix validation

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -48,12 +48,12 @@
       grabCursor: true,
       pagination: {
         el: ".swiper-pagination",
-        clickable: true,
+        clickable: true
       },
       navigation: {
         nextEl: ".swiper-arrow-next",
         prevEl: ".swiper-arrow-prev",
-        disabledClass: "arrow--disabled",
+        disabledClass: "arrow--disabled"
       }
     });
   });
